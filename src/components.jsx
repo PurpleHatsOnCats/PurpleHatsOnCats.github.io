47dae10ee988f1886b36f925344ec6e302f26fc9
PurpleHatsOnCats <seth.vandivere@gmail.com>
2026-01-02 19:10:36 -0500

    Almost done with card template

--- src/components.jsx
+++ src/components.jsx
@@ -4,41 +4,59 @@ function Project({ project }) {
             <div className="card-image">
                 <figure className="image is-4by3">
                     <img
-                        src="https://bulma.io/assets/images/placeholders/1280x960.png"
-                        alt="Placeholder image"
+                        src={"./media/projects/" + project.media[0].src}
+                        alt={project.media[0].alt}
                     />
                 </figure>
             </div>
             <div className="card-content">
                 <p className="title is-4">{project.title}</p>
-                <p className="subtitle is-5">{project.type}</p>
-                <ul>
-                    <li>
+                <p className="subtitle is-5">{project.type} - {project.time.season} {project.time.year}</p>
+                <span className="icons is-flex">
+
+                    {/* Team Icon */}
+                    <Rollover
+                        icon={getIconFromNumber(project.team.size)}
+                        iconText={project.team.size}
+                        rolloverText={<p>Team of {project.team.size} <br /> Roles: {project.team.roles.join(", ")}</p>}
+                    ></Rollover>
+
+                    {/* Length */}
+                    <Rollover
+                        icon={getIconFromText("Clock")}
+                        iconText={""}
+                        rolloverText={<p>Length of project: {project.time.length}</p>}
+                    ></Rollover>
+
+                    {/* Tools Icons */}
+                    {project.tools.map((tool) =>
                         <Rollover
-                            icon={getIconFromNumber(project.team.size)}
-                            iconText={project.team.size}
-                            rolloverText={"Roles: " + project.team.roles.join(", ")}
+                            icon={getIconFromText(tool)}
+                            iconText={""}
+                            rolloverText={tool}
                         ></Rollover>
-                    </li>
-                </ul>
+                    )}
+                </span>
                 <div className="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-                    iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-                    <a href="#">#responsive</a>
-                    <br />
-                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    <ul>
+                        {project.points.map((item) => (<li>{item}</li>))}
+                    </ul>
                 </div>
             </div>
+            <footer class="card-footer">
+                {returnIfTrue(project.buildPage, <a href={"#" + project.buildPage} class="card-footer-item">Build Page</a>)}
+                {returnIfTrue(project.infoPage, <a href={"#" + project.infoPage} class="card-footer-item">More Info</a>)}
+            </footer>
         </div>
     )
 };
 
-function Rollover({icon, iconText="", rolloverText=""}) {
+function Rollover({ icon, iconText = "", rolloverText = "" }) {
     return (
         <div className="rollover-container">
             <span className="rollover-icon">
                 <span className="icon">
-                    <i className={"fas " + icon}></i>
+                    {icon}
                 </span>
                 {iconText}
             </span>
@@ -49,12 +67,30 @@ function Rollover({icon, iconText="", rolloverText=""}) {
     );
 }
 
-const getIconFromNumber = (num) =>{
-    switch(num){
-        case 1: return "fa-user";
-        case 2: return "fa-user-group";
-        default: return "fa-users";
+const getIconFromNumber = (num) => {
+    let name;
+    switch (num) {
+        case 1: name = "fas fa-user"; break;
+        case 2: name = "fas fa-user-group"; break;
+        default: name = "fas fa-users"; break;
     }
+    return (<i className={name}></i>);
+}
+const getIconFromText = (text) => {
+    let name;
+    switch (text) {
+        case "Unity": name = "fa-brands fa-unity"; break;
+        case "Aseprite": return (<img src="./media/icons/aseprite-icon.png"></img>); break;
+        case "Clock": name = "fa-solid fa-clock"; break;
+        default: name = ""; break;
+    }
+    return (<i className={name}></i>);
+}
+const returnIfTrue = (bool, returnData) => {
+    if(bool){
+        return returnData;
+    }
+    return "";
 }
 
 export { Project };
