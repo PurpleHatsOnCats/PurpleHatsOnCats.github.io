4cc66c0e2044add9eb1c5d1133e833c80fc7a82e
PurpleHatsOnCats <seth.vandivere@gmail.com>
2026-01-07 23:36:10 -0500

    test 4.9

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { loadJSON } from "./load.js";
 import { Project } from "./components.jsx";
 import { StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
-import projects from './project-data/projects.json';
+//import projects from './project-data/projects.json';
 
-const projectDataLocation = "./project-data/projects.json";
+const projectDataLocation = "./src/project-data/projects.json";
 console.log(projectDataLocation);
-//const projects = (await loadJSON(projectDataLocation)).projects;
+const projects = (await loadJSON(projectDataLocation)).projects;
 console.log(projects);
 
 createRoot(document.getElementById('root')).render(
