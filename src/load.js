57c51ee9fe64fd5784afdb2a2639c97bd8d48eb3
PurpleHatsOnCats <seth.vandivere@gmail.com>
2026-01-07 22:49:20 -0500

    test 4.2

--- src/load.js
+++ src/load.js
@@ -1,5 +1,6 @@
 async function loadJSON (url){
     let returnValue;
+    console.log("Fetching from: " + url);
     await fetch(url).
     then((response) => response.json()).then((json) =>{
         returnValue = json;
